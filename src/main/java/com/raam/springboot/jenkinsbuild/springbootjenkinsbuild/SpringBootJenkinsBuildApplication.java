195c7dcc81136ac69f9d8fe50dd165b0ab95c462
raamc21 <raam.c21@gmail.com>
2019-01-01 19:56:43 -0600

    test project

---
+++ src/main/java/com/raam/springboot/jenkinsbuild/springbootjenkinsbuild/SpringBootJenkinsBuildApplication.java
@@ -0,0 +1,14 @@
+package com.raam.springboot.jenkinsbuild.springbootjenkinsbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsBuildApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/raam/springboot/jenkinsbuild/springbootjenkinsbuild/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.raam.springboot.jenkinsbuild.springbootjenkinsbuild.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String Hello() {
+		return ("Hello users.......");
+	}
+	
+
+}
